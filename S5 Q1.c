d69376d954c96da0e2c951861c4d0727e2b256ca
AdeelRehman123 <adeelrahmanabro@gmail.com>
2024-09-28 09:57:11 -0700

    Add files via upload

---
+++ S5 Q1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int year;
+	printf("Enter Year:");
+	scanf("%d",&year);
+	if(year%400==0 || year%4==0 && year%100!=0)
+	{
+		printf("This Year is a LeapYear");
+	} 
+    else
+	{
+		printf("This is not leapYear");
+	}
+	return 0;
+}
